367e844a59e6f063021a4bfff5d6d4dc51d9b603
arfoysal <arfoysal.me@gmail.com>
2023-09-22 23:13:42 +0600

    Add advance locator class for custom xpath and CSS selector

--- src/test/java/org/example/DriverSetup.java
+++ src/test/java/org/example/DriverSetup.java
@@ -7,12 +7,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class DriverSetup {
     public WebDriver driver;
     @BeforeSuite
     public WebDriver setDriver(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return  driver;
     }
 
